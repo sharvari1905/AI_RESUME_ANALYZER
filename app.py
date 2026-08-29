3af100c1bd2c224bd1db1c2b0294ce11987e8076
sharvari1905 <patilsharvari713@gmail.com>
2026-08-29 11:45:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Resume Analyzer",
+    page_icon="📄",
+    layout="centered"
+)
+
+st.title("📄 AI Resume Analyzer")
+
+st.write("hello")
+
+st.success("hello")
